00456737a1358bab21fef16521cf4a8568afd69a
Abdoulllahi <dabdoulahi@gmail.com>
2022-11-29 20:38:24 +0000

    day5 completed

--- Day5/exercise1_average_height.py
+++ Day5/exercise1_average_height.py
@@ -1,19 +1,19 @@
 '''
  # @ Author: Abdou Lahi DIOP
- # @ Create Time: 2022-11-28 22:04:14
+ # @ Create Time: 2022-11-29 11:19:29
  # @ Modified by: Abdou Lahi DIOP
- # @ Modified time: 2022-11-29 11:31:49
+ # @ Modified time: 2022-11-29 11:28:00
  # @ Description:
  '''
 
 # 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+    student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
 
-#Write your code below this row 👇
+# Write your code below this row 👇
 heights = 0
 for height in student_heights:
     heights += height
