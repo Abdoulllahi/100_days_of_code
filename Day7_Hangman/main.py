ae2c21ab982c8bc1d2ad82093948d9809f3baa2e
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-02 14:45:53 +0000

    keeping track of the player's live

--- Day7_Hangman/main.py
+++ Day7_Hangman/main.py
@@ -21,24 +21,102 @@ import random
 #     file.seek(0)
 #     json.dump(data, file, indent=4)
 #     file.truncate()
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 level = input("Please choose a level: easy, medium or hard\n")
 
 with open("Day7_Hangman/words_english.json", "r") as file:
     data = json.load(file)
-    if (level == "easy"):
+    if level == "easy":
         words = [word for word in data.keys() if data[word] in range(1, 8)]
-    elif (level == "medium"):
+    elif level == "medium":
         words = [word for word in data.keys() if data[word] in range(8, 15)]
-    elif (level == "hard"):
+    elif level == "hard":
         words = [word for word in data.keys() if data[word] > 15]
     file.close()
-random_word = random.choice(words)
-guess = input("Guess a letter: ").lower()
 
-for letter in random_word:
-    if (letter == guess):
-        print("Right")
-    else:
-        print("Wrong")
-print(random_word)
+random_word = random.choice(words)
+display = []
+end_game = False
+lives = 6
+
+for _ in range(len(random_word)):
+        display += '_'
+
+while not end_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess not in random_word or guess in display:
+        lives -= 1
+        
+    for position in range(len(random_word)):
+        if random_word[position] == guess:
+            display[position] = guess
+        
+    if lives == 0:
+        print(stages[0])
+        print("You lose!!!")
+        break
+    
+    print(random_word)
+    print(display)
+    print(stages[lives])
+
+    if "_" not in display and not lives == 0:
+        end_game = True
+        print("You win!")
