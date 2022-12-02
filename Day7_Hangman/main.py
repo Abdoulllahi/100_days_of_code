3f050bd98af652895e73b83572e5c1caa5e3bad8
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-02 15:39:42 +0000

    deleting pycache

--- Day7_Hangman/main.py
+++ Day7_Hangman/main.py
@@ -13,7 +13,6 @@
 import json
 import random
 from hangman_art import stages, logo
-
 # @ This script was created to add words length as a value in the initial json file
 # with open("Day7_Hangman/words_english.json", "r+") as file:
 #     data = json.load(file)
