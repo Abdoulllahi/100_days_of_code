46229424ab150371a1a6134f260ad381206b0edf
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-05 00:59:07 +0000

    caesar cypher : reorganizing code

--- Day8_Beginner_functions_ceasar_cypher/Caesar_Cipher/main.py
+++ Day8_Beginner_functions_ceasar_cypher/Caesar_Cipher/main.py
@@ -31,12 +31,12 @@ shift = int(input("Type the shift number:\n"))
 # a message.
 
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        new_letter = alphabet[(alphabet.index(letter) + shift_amount) % 26]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         new_letter = alphabet[(alphabet.index(letter) + shift_amount) % 26]
+#         cipher_text += new_letter
+#     print(f"The encoded text is {cipher_text}")
 
 ### Decryption ###
 # TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
@@ -53,17 +53,32 @@ def encrypt(plain_text, shift_amount):
 # TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable.
 # Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND*
 # decrypt a message.
-def decrypt(cipher_text, shift_amount):
-    decrypted_text = ""
-    for letter in cipher_text:
-        old_letter = alphabet[(alphabet.index(letter) - shift_amount) % 26]
-        decrypted_text += old_letter
-    print(f"The decoded text is: {decrypted_text}")
+# def decrypt(cipher_text, shift_amount):
+#     decrypted_text = ""
+#     for letter in cipher_text:
+#         old_letter = alphabet[(alphabet.index(letter) - shift_amount) % 26]
+#         decrypted_text += old_letter
+#     print(f"The decoded text is: {decrypted_text}")
 
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
-else:
-    print("Bad entry")
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#     decrypt(cipher_text=text, shift_amount=shift)
+# else:
+#     print("Bad entry")
+
+### Caesar ###
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+def caesar(start_text, shift_amount, cipher_direction):
+    final_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        final_letter = alphabet[(alphabet.index(letter) + shift_amount) % 26]
+        final_text += final_letter
+    print(f"The {cipher_direction}d text is : {final_text}")
+
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
