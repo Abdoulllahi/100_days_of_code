433613f33a45376326966dbc934140128e8c023a
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-08 22:53:52 +0000

    just commiting

--- Day1_Beginner_variables/exercise1_printing.py
+++ Day1_Beginner_variables/exercise1_printing.py
@@ -1,3 +1,11 @@
+'''
+ # @ Author: Abdou Lahi DIOP
+ # @ Create Time: 2022-11-24 13:56:58
+ # @ Modified by: Abdou Lahi DIOP
+ # @ Modified time: 2022-12-08 22:53:31
+ # @ Description:
+ '''
+
 #Write your code below this line 👇
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
