9ad8e5d5fc67482edc4ad0ae62080793a81c7419
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-20 15:45:40 +0000

    QuizBrain class and next_question method

--- day17-intermediate-quizz-project/main.py
+++ day17-intermediate-quizz-project/main.py
@@ -1,11 +1,22 @@
+'''
+ # @ Author: Abdou Lahi DIOP
+ # @ Create Time: 2022-12-17 13:50:59
+ # @ Modified by: Abdou Lahi DIOP
+ # @ Modified time: 2022-12-20 15:42:55
+ # @ Description:
+ '''
+
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
+# TODO: asking the question
 
-#TODO: asking the question
+# TODO: Check if the question is correct
 
-#TODO: Check if the question is correct
+# TODO: Check if we are at the end of the quiz
 
-#TODO: Check if we are at the end of the quiz
-
-question_bank = [Question(text=question["text"], answer=question["answer"]) for question in question_data]
+question_bank = [Question(text = question["text"], answer = question["answer"])
+                 for question in question_data]
+quiz = QuizBrain(question_list = question_bank)
+quiz.next_question()
