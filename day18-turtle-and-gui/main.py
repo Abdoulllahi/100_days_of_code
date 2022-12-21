3012d8752cf5504d33d963b57b9da591e6eeb72e
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-21 21:51:16 +0000

    drawing different shapes

--- day18-turtle-and-gui/main.py
+++ day18-turtle-and-gui/main.py
@@ -1,13 +1,34 @@
+import random
 from turtle import Turtle, Screen
 
 
 turtle = Turtle()
 
-turtle.shape("turtle")
-turtle.color("red", "green")
-for _ in range(4):
-    turtle.forward(100)
-    turtle.left(90)
 
-screen = Screen()
-screen.exitonclick()
+# turtle.hideturtle()
+# for _ in range(25):
+#     turtle.forward(5)
+#     turtle.penup()
+#     turtle.forward(5)
+#     turtle.pendown()
+# for _ in range(4):
+#     turtle.forward(100)
+#     turtle.left(90)
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", 
+           "SeaGreen"]
+
+def draw_figure(sides):
+    turtle.shape()
+    # turtle.color("black", "red")
+    for _ in range(sides):
+        angle = 360 / sides
+        turtle.forward(100)
+        turtle.right(angle)
+    # screen = Screen()
+    # screen.exitonclick()        
+
+for shape_side_n in range(3, 10):
+    turtle.color(random.choice(colours))
+    draw_figure(shape_side_n)
+
+draw_figure(10)
