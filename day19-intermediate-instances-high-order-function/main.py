30d0f99b5f80cc145dfa9c7ebbc071e951321b9c
Abdoulllahi <dabdoulahi@gmail.com>
2022-12-26 13:01:26 +0000

    make an etch-a-sketch

--- day19-intermediate-instances-high-order-function/main.py
+++ day19-intermediate-instances-high-order-function/main.py
@@ -13,9 +13,40 @@ screen = Screen()
 
 
 def move_forward():
-    tim.forward(100)
+    tim.forward(50)
 
+
+
+def move_backward():
+    tim.backward(50)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+    
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+    
+    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+# screen.listen()
+# screen.onkey(fun=move_forward, key="space")
+# screen.exitonclick()
+
+#TODO: Z = Forwards, B = Backwards, A = Counter-Clockwise, D = Clockwise, C = Clear Drawing
 
 screen.listen()
-screen.onkey(fun=move_forward, key="space")
+screen.onkey(fun=move_forward, key="z")
+screen.onkey(fun=move_backward, key="b")
+screen.onkey(fun=turn_right, key="d")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=clear, key="c")
 screen.exitonclick()
